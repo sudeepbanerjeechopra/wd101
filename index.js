6afa96d100c74bacefef1d492b812dcbe80f7350
SUDEEP BANERJEE CHOPRA <97747718+sudeepbanerjeechopra@users.noreply.github.com>
2023-11-21 21:01:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,3 +39,53 @@ const EntriesRetrieved = ()=> {
    return entries;
 }
 
+let userentries=EntriesRetrieved();
+
+const displayEntries = ()=>
+{
+   const entries=EntriesRetrieved();
+   const tableEntries =entries.map((entry)=>{
+      const nameCell= `<td class='border px-4 py-2'>${entry.name}</td>`;
+        const emailCell= `<td class='border px-4 py-2'>${entry.email}</td>`;
+        const passwordCell= `<td class='border px-4 py-2'>${entry.password}</td>`;
+        const dobCell= `<td class='border px-4 py-2'>${entry.dob}</td>`;
+        const acceptTermsCell= `<td class='border px-4 py-2'>${entry.acceptedTermsAndConditions}</td>`;
+        const row= `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
+        return row;
+   }).join("\n");
+
+   const table= `<table class="table-auto w-full"><tr>
+    <th class="px-4 py-2">Name</th>
+    <th class="px-4 py-2">Email</th>
+    <th class="px-4 py-2">Password</th>
+    <th class="px-4 py-2">Dob</th>
+    <th class="px-4 py-2">Accepted terms?</th>
+    </tr>${tableEntries} </table>`;
+    let details=document.getElementById("user-entries");
+    details.innerHTML = table;
+
+   
+}
+const saveUserform = (event) =>{
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const dob = document.getElementById('dob').value;
+    const acceptedTermsAndConditions = document.getElementById('acceptTerms').checked;
+
+    const entry={
+       name,
+       email,
+       password,
+       dob,
+       acceptedTermsAndConditions
+    };
+    userentries.push(entry);
+
+    localStorage.setItem("user-entries",JSON.stringify(userentries));
+    displayEntries();
+}
+
+userform.addEventListener("submit",saveUserform);
+displayEntries();
