f61a52dc55768323901658eb5396849f92ef078d
SUDEEP BANERJEE CHOPRA <97747718+sudeepbanerjeechopra@users.noreply.github.com>
2023-11-21 20:59:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ function checkAgeValidity() {
    const month = dobDate.getMonth();
    const day = dobDate.getDate();
    let age = today.getFullYear() - dobDate.getFullYear();
+
+   let checkmonth = today.getMonth() < month
+   let checkday = today.getMonth() === month && today.getDate() < day;
+    
+ 
+   if (checkmonth || checkday) {
+     age--;
+   }
+ 
+   const isValidAge = age > 18 && age < 55;
+ 
+   if (!isValidAge) {
+     dobElement.setCustomValidity("The age should be between 18 and 55 years");
+     dobElement.reportValidity();
+   } else {
+     dobElement.setCustomValidity("");
+   }
+ }
