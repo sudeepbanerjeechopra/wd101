bd7fa7d89e89ffcc267704dd14013bb5706d74f3
SUDEEP BANERJEE CHOPRA <97747718+sudeepbanerjeechopra@users.noreply.github.com>
2023-11-21 20:59:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-i
+function checkAgeValidity() {
+   const dobElement = document.getElementById("dob");
+   const dobValue = dobElement.value;
+   const dobDate = new Date(dobValue);
+ 
+   const today = new Date();
+   const month = dobDate.getMonth();
+   const day = dobDate.getDate();
+   let age = today.getFullYear() - dobDate.getFullYear();
